c06bc8de72c9584b5aa6133c419dfbd85363d702
Jarosław Żołnowski <jarek@devintent.com>
2023-04-20 06:40:16 +0200

    chore(npm): update dependencies to the latest versions
    * migrate gulp to v4
    * update versions in the README

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,26 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
-var cleanCss = require('gulp-clean-css');
-var rename = require('gulp-rename');
+const { src, dest, series, watch } = require('gulp');
+const gulpSass = require('gulp-sass')(require('sass'));
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
-var paths = {
+const paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./www/css/'))
+const sass = (done) => {
+  return src('./scss/ionic.app.scss')
+    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(dest('./www/css/'))
     .pipe(cleanCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(dest('./www/css/'))
     .on('end', done);
-});
+};
 
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.sass, ['sass']);
-});
+const watching = () => watch(paths.sass, series(sass));
+
+exports.default = sass;
+exports.sass = sass;
+exports.watch = watching;
